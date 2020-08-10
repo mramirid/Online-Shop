dbbb5970e5e379d765c7d65d2a57cdff1eb687fc
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-08-10 17:10:09 +0700

    Specific validation with body() instead of check()

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { check } from 'express-validator'
+import { check, body } from 'express-validator'
 
 import * as authController from '../controllers/auth'
 
@@ -9,15 +9,20 @@ router.get('/signup', authController.getSignup)
 
 router.post(
   '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email')
-    .custom((value, {req}) => {
-      if (value === 'test@test.com') {
-        throw new Error('This email address is forbidden')
-      }
-      return true
-    }),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email address is forbidden')
+        }
+        return true
+      }),
+    body('password', 'Enter password with only number or text & at least 5 characters')
+      .isLength({ min: 5, max: 30 })
+      .isAlphanumeric()
+  ],
   authController.postSignup
 )
 
